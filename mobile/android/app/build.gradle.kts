3e7bd3fb6b5a0bf1db68f19ae011aadbd2413d39
akouedekon <lakouedekon@gmail.com>
2026-08-05 21:07:42 +0200

    mobile: fix AGP version to 8.1.2 + gradle optimization for local build
    Note: Local builds on Windows with GraalVM JDK 17 + AGP 9.x + Java 21 have a known bug.
    CI builds on Ubuntu should work correctly. APK will be available from GitHub Actions artifacts.

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.kovo_mobile"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     defaultConfig {
